2363d23a67cfe58802e9eb2e774ade8ae3c066d4
Jinbin_z <Jinbin_z@163.com>
2019-12-04 10:58:27 +0800

    feign搭建

---
+++ feign/src/main/java/com/jinbin/feign/HiController.java
@@ -0,0 +1,24 @@
+package com.jinbin.feign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jinbin_z
+ * @description
+ * @date 2019/12/3 17:52
+ */
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
---
+++ feign/src/main/java/com/jinbin/feign/SchedualServiceHi.java
@@ -0,0 +1,18 @@
+package com.jinbin.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Jinbin_z
+ * @description
+ * @date 2019/12/3 17:51
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
